a142f35b6405e2f63adeffcb95e94977e63301f0
Nisse <nielsvaes@gmail.com>
2021-12-16 22:19:37 +0100

    updated payloads, needs some work

--- constants.lua
+++ constants.lua
@@ -57,6 +57,114 @@ DEFAULTS = {
     spawn_max = 4
 }
 
+PAYLOADS = {}
+PAYLOADS.AIRPLANES = {}
+PAYLOADS.AIRPLANES.FA18 = {
+    {
+        [1] = {
+            ["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+        },
+        [2] = {
+            ["CLSID"] = "{FPU_8A_FUEL_TANK}",
+        },
+        [3] = {
+            ["CLSID"] = "LAU-115_2*LAU-127_AIM-9L",
+        },
+        [4] = {
+            ["CLSID"] = "LAU-115_2*LAU-127_AIM-9L",
+        },
+        [5] = {
+            ["CLSID"] = "{FPU_8A_FUEL_TANK}",
+        },
+        [6] = {
+            ["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+        },
+        [7] = {
+            ["CLSID"] = "{FPU_8A_FUEL_TANK}",
+        },
+        [8] = {
+            ["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+        },
+        [9] = {
+            ["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+        },
+    },
+    {
+        [1] = {
+            ["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+            ["num"] = 7,
+        },
+        [2] = {
+            ["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+            ["num"] = 3,
+        },
+        [3] = {
+            ["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+            ["num"] = 8,
+        },
+        [4] = {
+            ["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+            ["num"] = 2,
+        },
+        [5] = {
+            ["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+            ["num"] = 4,
+        },
+        [6] = {
+            ["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+            ["num"] = 6,
+        },
+        [7] = {
+            ["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+            ["num"] = 1,
+        },
+        [8] = {
+            ["CLSID"] = "{5CE2FF2A-645A-4197-B48D-8720AC69394F}",
+            ["num"] = 9,
+        },
+    },
+    {
+        [1] = {
+            ["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+            ["num"] = 1,
+        },
+        [2] = {
+            ["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+            ["num"] = 9,
+        },
+        [3] = {
+            ["CLSID"] = "{LAU-115 - AIM-7M}",
+            ["num"] = 2,
+        },
+        [4] = {
+            ["CLSID"] = "{8D399DDA-FF81-4F14-904D-099B34FE7918}",
+            ["num"] = 4,
+        },
+        [5] = {
+            ["CLSID"] = "{FPU_8A_FUEL_TANK}",
+            ["num"] = 5,
+        },
+        [6] = {
+            ["CLSID"] = "{8D399DDA-FF81-4F14-904D-099B34FE7918}",
+            ["num"] = 6,
+        },
+        [7] = {
+            ["CLSID"] = "{LAU-115 - AIM-7M}",
+            ["num"] = 8,
+        },
+        [8] = {
+            ["CLSID"] = "{FPU_12_FUEL_TANKHighVis}",
+            ["num"] = 3,
+        },
+        [9] = {
+            ["CLSID"] = "{FPU_12_FUEL_TANKHighVis}",
+            ["num"] = 7,
+        },
+    }
+}
+
+
+
 
 
 MESSAGE:New("Constants loaded", 5):ToAll()
