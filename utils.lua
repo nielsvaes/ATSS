b1335076563f86d764481df8ba5d64ce7c195b12
Nisse <nielsvaes@gmail.com>
2021-12-18 09:17:53 +0100

    adding deepcopy

--- utils.lua
+++ utils.lua
@@ -278,5 +278,28 @@ end
 
 
 
+---deepcopy
+---@param o table
+---@param seen table don't pass in argument here, used in recursive calling
+function ATSS_UTILS.deepcopy(o, seen)
+  seen = seen or {}
+  if o == nil then return nil end
+  if seen[o] then return seen[o] end
+
+  local no
+  if type(o) == 'table' then
+    no = {}
+    seen[o] = no
+
+    for k, v in next, o, nil do
+      no[deepcopy(k, seen)] = deepcopy(v, seen)
+    end
+    setmetatable(no, deepcopy(getmetatable(o), seen))
+  else -- number, string, boolean, etc
+    no = o
+  end
+  return no
+end
+
 
 MESSAGE:New("Utils loaded", 5):ToAll()
